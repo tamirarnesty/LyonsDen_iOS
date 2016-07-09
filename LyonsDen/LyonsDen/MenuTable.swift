15de9ef297aac4f96a1bb23ef32eedafc0692e9a
tamirsway <tamiryuval@live.ca>
2016-07-09 19:48:35 -0400

    a few changes

--- LyonsDen/LyonsDen/MenuTable.swift
+++ LyonsDen/LyonsDen/MenuTable.swift
@@ -10,10 +10,10 @@ import UIKit
 
 class MenuTable: UITableViewController {
     // An array containig the titles of the cell in [0]
-    let titles = ["Home", "Events", "Clubs", "Calendar", "Contact"]
+    let titles = ["DEN", "Home", "Announcements", "Calendar", "Clubs", "Events", "Contact"]
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return titles.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -21,6 +21,12 @@ class MenuTable: UITableViewController {
         cell.backgroundColor = UIColor(red: 0.0118, green: 0.2431, blue: 0.5765, alpha: 1)
         cell.textLabel?.text = titles[indexPath.row]
         cell.textLabel?.textColor = UIColor(red: 0.9961, green: 0.7765, blue: 0.2784, alpha: 1)
+        if indexPath.row == 0 {
+            cell.textLabel?.font = UIFont(name: "Reckoner", size: 41.0)
+            cell.selectionStyle = UITableViewCellSelectionStyle.None
+        } else {
+        cell.textLabel?.font = UIFont(name: "Reckoner", size: 26.0)
+        }
         return cell
     }
     
