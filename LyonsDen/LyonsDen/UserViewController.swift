9a482942f2e96d5ea05374c06441b9b35ae3b35c
tamirsway <tamiryuval@live.ca>
2016-08-23 01:32:11 -0400

    - User can now sign out, delete account, and send password reset email (last two not tested yet). - User can edit display name, email, and password. - Auto log in works with all updated information (email update not tested yet for auto log in or database). - MenuTable incorporates user display name.

--- LyonsDen/LyonsDen/UserViewController.swift
+++ LyonsDen/LyonsDen/UserViewController.swift
@@ -9,7 +9,12 @@
 import Foundation
 import FirebaseAuth
 
-class UserViewController: UIViewController {
+class UserViewController: UIViewController, UITextViewDelegate {
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    @IBOutlet var userInfo: [UITextView]!
+    @IBOutlet weak var rightBarButton: UIBarButtonItem!
+    //------------ fix constraints and make it work
+    var displayNameText:String! = nil
     
     @IBAction func signOutPressed(sender: AnyObject) {
         try! FIRAuth.auth()?.signOut()
@@ -17,8 +22,105 @@ class UserViewController: UIViewController {
         self.performSegueWithIdentifier("signOutSegue", sender: self)
     }
 
+    @IBAction func deleteAccount(sender: AnyObject) {
+        FIRAuth.auth()?.currentUser?.deleteWithCompletion { error in
+            if let error = error {
+                print("Something went wrong")
+            } else {
+               (UIApplication.sharedApplication().delegate as! AppDelegate).displayError("Success", errorMsg: "\(FIRAuth.auth()?.currentUser?.displayName) has been deleted.")
+            }
+        }
+    }
+    
+    @IBAction func resetPassword(sender: AnyObject) {
+        
+        FIRAuth.auth()?.sendPasswordResetWithEmail((FIRAuth.auth()?.currentUser?.email)!) { error in
+            if let error = error {
+                print ("Something went wrong")
+            } else {
+                (UIApplication.sharedApplication().delegate as! AppDelegate).displayError("Success", errorMsg: "A password reset email was sent to \(FIRAuth.auth()?.currentUser?.displayName)")
+            }
+        }
+    }
+    
+    @IBAction func updateUser(sender: AnyObject) {
+        let user = FIRAuth.auth()?.currentUser
+        if let user = user {
+            let changeRequest = user.profileChangeRequest()
+            changeRequest.displayName = userInfo[0].text!
+            changeRequest.commitChangesWithCompletion { error in
+                if let error = error {
+                    print("Something went wrong.")
+                } else {
+                    NSUserDefaults.standardUserDefaults().setValue(self.userInfo[0].text, forKey: "displayName")
+                    print("Successful name update.")
+                }
+            }
+            user.updateEmail(userInfo[1].text!) { error in
+                if let error = error {
+                    print("Something went wrong.")
+                } else {
+                    NSUserDefaults.standardUserDefaults().setValue(self.userInfo[1].text, forKey: "uID")
+                    print("Successful email update.")
+                }
+            }
+            if userInfo[2].text != "Secure Information" {
+            user.updatePassword(userInfo[2].text!) { error in
+                if let error = error {
+                    print("Something went wrong.")
+                } else {
+                    NSUserDefaults.standardUserDefaults().setObject(self.userInfo[2].text, forKey: "Pass")
+                    print("Successful password update.")
+                }
+            }
+            }
+            
+            for text in userInfo {
+                text.editable = false
+            }
+            
+            (UIApplication.sharedApplication().delegate as! AppDelegate).displayError("Success", errorMsg: "All updates have been submitted.")
+        }
+    }
+    
+    @IBAction func editInfo(sender: AnyObject) {
+        // make edit have to verify with touchID or current password.
+        for text in userInfo {
+            text.editable = true
+        }
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textView: UITextView) -> Bool{
+        textView.resignFirstResponder()
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Make sidemenu swipeable
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        
+        for text in userInfo {
+            text.editable = false
+            text.layer.borderWidth = 0.5
+            text.layer.borderColor = UIColor.yellowColor().CGColor
+        }
+
+        
+        if let displayName = NSUserDefaults.standardUserDefaults().objectForKey("displayName") as? String {
+            displayNameText = displayName
+        }
+        userInfo[0].text = (FIRAuth.auth()?.currentUser?.displayName != nil) ? FIRAuth.auth()?.currentUser?.displayName : displayNameText
+        userInfo[1].text = FIRAuth.auth()?.currentUser?.email
+
     }
     
     override func didReceiveMemoryWarning() {
